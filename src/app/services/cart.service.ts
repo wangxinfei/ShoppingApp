572b4fac3764d0879e54f938dcba0ad4ae8b9cd9
Fiona <wangxinfei123456@outlook.com>
2023-09-29 17:13:05 -0400

    product details

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../products';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  items: Product[] = []
+  constructor() { }
+}
